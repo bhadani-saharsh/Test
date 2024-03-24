5cd4ef54ff53e4799e490c6bf529542551424595
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2024-03-24 19:48:49 +0530

    Second Commit
    Second Commit

--- Test/settings.py
+++ Test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1!gj)7)ds&@^tel9gi5ic950lg_+t^(htv@#go8kgee+h_6tg(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
