ac8781179e09e8e762aca4ac87bf5921cd1e0740
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2024-05-04 20:11:33 +0530

    static fix 8
    static fix 8

--- AppointmentManagementSystem/settings.py
+++ AppointmentManagementSystem/settings.py
@@ -15,6 +15,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
+
+print("BASE_DIR content: "+str(os.listdir(str(BASE_DIR))))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
