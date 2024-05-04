d30fbc9d1617df2f46935d2a7b18e62b61731c56
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2024-05-04 20:14:15 +0530

    static fix 9
    static fix 9

--- AppointmentManagementSystem/settings.py
+++ AppointmentManagementSystem/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 print("BASE_DIR content: "+str(os.listdir(str(BASE_DIR))))
 
