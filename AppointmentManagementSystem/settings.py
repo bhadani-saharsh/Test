288a222d454258f65e6e45b2513f408641410f27
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2024-05-04 10:31:35 +0530

    Update settings.py

--- AppointmentManagementSystem/settings.py
+++ AppointmentManagementSystem/settings.py
@@ -115,12 +115,12 @@ USE_I18N = True
 USE_TZ = True
 
 STATICFILES_DIRS = [
-    BASE_DIR / "mystaticfiles"
+    "../mystaticfiles"
 ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, "/AppointmentManagementSystem/mystaticfiles/")
-STATIC_URL = os.path.join(BASE_DIR, "/mystaticfiles/")
+STATIC_URL = "../mystaticfiles/"
 print("STATIC_URL: "+STATIC_URL)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
