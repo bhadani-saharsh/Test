42a8d7ec8c735eeb72a4761e983f310ec079cf36
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2024-05-04 18:49:10 +0530

    static url fix
    static url fix

--- AppointmentManagementSystem/settings.py
+++ AppointmentManagementSystem/settings.py
@@ -121,8 +121,8 @@ STATICFILES_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, "/AppointmentManagementSystem/mystaticfiles/")
-STATIC_URL = '/mystaticfiles/'
-
+STATIC_URL = os.path.join(BASE_DIR, "mystaticfiles\\").replace("\\","/")
+print("STATIC_URL: "+STATIC_URL)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
