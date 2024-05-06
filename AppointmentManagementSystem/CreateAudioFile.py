049ecdf5e7b6f6dee0fc6feac3043fef402b0d76
bhadani-saharsh <164743737+bhadani-saharsh@users.noreply.github.com>
2024-05-06 06:49:11 +0530

    Final fix 1
    Final fix 1

--- AppointmentManagementSystem/CreateAudioFile.py
+++ AppointmentManagementSystem/CreateAudioFile.py
@@ -151,7 +151,7 @@ def create_credentials():
     # created automatically when the authorization flow completes for the first
     # time.
     # Saharsh -- need to change this to run on server
-    sub_folder = "\\AppointmentManagementSystem\\"
+    sub_folder = "/AppointmentManagementSystem/"
     if os.path.exists(os.getcwd()+sub_folder+"gdrive-token.json"):
         creds = Credentials.from_authorized_user_file(filename=os.getcwd()+sub_folder+"gdrive-token.json", scopes=SCOPES)
     # If there are no (valid) credentials available, let the user log in.
